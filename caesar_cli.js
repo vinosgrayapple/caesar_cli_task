0c5a0cffd419cdd10c19cb3e4044e618519f46b7
Serhii Komarychev <skomarychev@gmail.com>
2020-10-01 08:53:43 +0300

    initial

---
+++ caesar_cli.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+const parseArgvs = require('./lib/parseArgs')(process.argv)
+const pipelineFn = require('./lib/pipeline')
+const checkOpton = require('./lib/checkOption')
+
+const { action = false, shift = false, input = false, output = false } = parseArgvs.opts()
+
+checkOpton(action, shift, input, output, parseArgvs)
+pipelineFn(action, shift, input, output)
