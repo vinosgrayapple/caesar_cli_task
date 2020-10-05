df813c4d40170e7e6b428a95d4c5c6a3204bc934
Serhii Komarychev <komarichev@gmail.com>
2020-10-05 21:28:43 +0300

    change dir

--- task1/caesar_cli.js
+++ task1/caesar_cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-const parseArgvs = require('./lib/parseArgs')(process.argv)
-const pipelineFn = require('./lib/pipeline')
-const checkOpton = require('./lib/checkOption')
+const parseArgvs = require('../lib/parseArgs')(process.argv)
+const pipelineFn = require('../lib/pipeline')
+const checkOpton = require('../lib/checkOption')
 
 const { action = false, shift = false, input = false, output = false } = parseArgvs.opts()
 
